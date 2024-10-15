f2a9608176912a028caf8913d4ba49371bfee2ad
Miguel Fabián Salazar <miguel@fabiansalazar.es>
2024-10-15 16:11:45 +0200

    get_tariffs_nmf fix function calls to dplyr

--- R/get_timeseries_helper_functions.R
+++ R/get_timeseries_helper_functions.R
@@ -12,16 +12,16 @@ get_tariffs_nmf <- function(
   # retrieve the code corresponding to the economy passed as argument in .economy, if necessary, or return if error
   .economy_code <- .economy
 
-  if(.economy %in% (wtor::get_reporting_economies() |> _$name)) {
-    .economy_code <- wtor::get_reporting_economies() |> filter(name == .economy) |> _$code
+  if(.economy %in% (get_reporting_economies() |> _$name)) {
+    .economy_code <- get_reporting_economies() |> dplyr::filter(name == .economy) |> _$code
   }
 
-  if(!.economy_code %in% (wtor::get_reporting_economies() |> _$code)) {
+  if(!.economy_code %in% (get_reporting_economies() |> _$code)) {
     stop(sprintf("Economy %s is not a valid reporting economy code or name", .economy_code))
   }
 
   # retrieve NMF tariffs data
-  .tariffs_nmf_df <- wtor::get_timeseries_data(
+  .tariffs_nmf_df <- get_timeseries_data(
     code = "HS_A_0015",
     reporting_economies = .economy_code,
     products_or_sectors = "all",
@@ -33,27 +33,29 @@ get_tariffs_nmf <- function(
   if(.full_names) {
 
     # claude 3.5 code
-    hs6_code_names_df <- wtor::get_products_sectors("HS") |>
-      mutate(
+    hs6_code_names_df <- get_products_sectors("HS") |>
+      dplyr::mutate(
         level1 = substr(code, 1, 2),
         level2 = substr(code, 1, 4),
         level3 = code
-      ) %>%
+      )
+
+    hs6_code_names_df |>
       # Join the tibble with itself to get descriptions for each level
-      left_join(select(., code, name), by = c("level1" = "code"), suffix = c("", "_level1")) %>%
-      left_join(select(., code, name), by = c("level2" = "code"), suffix = c("", "_level2")) %>%
+      dplyr::left_join(dplyr::select(hs6_code_names_df, code, name), by = c("level1" = "code"), suffix = c("", "_level1")) |>
+      dplyr::left_join(dplyr::select(hs6_code_names_df, code, name), by = c("level2" = "code"), suffix = c("", "_level2")) |>
       # Combine descriptions for rows with 6-character codes
-      mutate(
-        full_name= case_when(
+      dplyr::mutate(
+        full_name= dplyr::case_when(
           nchar(code) == 6 ~ paste(name_level1, name_level2, name, sep = " - "),
           TRUE ~ name
         )
       ) |>
-      select(code, full_name)
+      dplyr::select(code, full_name)
 
     .tariffs_nmf_df <- .tariffs_nmf_df |>
-      left_join(
-        hs6_code_names_df |> rename(productorsectorcode= code), by="productorsectorcode"
+      dplyr::left_join(
+        hs6_code_names_df |> dplyr::rename(productorsectorcode= code), by="productorsectorcode"
       )
 
 
